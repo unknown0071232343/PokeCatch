674d23b94702ce6342dd849e2c6ac925a4c81e3b
unknown0071232343 <123096144+unknown0071232343@users.noreply.github.com>
2023-01-19 12:30:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-poketwo_prefix = "p!"
-spam_channel_id = 0
-choose_pokemons = False
+poketwo_prefix = "@poketwo"
+spam_channel_id = 1064149786242064426
+choose_pokemons = true
 list_poke_for_you = False # Instead of catching the pokemon, it will tell you which it is
-catching_poke_channels = 0 # Change this to your channel id where you catch pokemon
+catching_poke_channels = 1064149786242064426# Change this to your channel id where you catch pokemon
